941eb594e0960155b70ff1b0d50d83d0059bd8db
0xv1n <gdejes@protonmail.ch>
2026-01-16 19:33:29 -0800

    Update descriptions.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -12,7 +12,7 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 
 const prod = (process.argv[2] === "production");
-const dir = "build/hevy-for-obsidian";
+const dir = "build/hevy";
 
 const copyRequiredFiles = () => {
     try {
